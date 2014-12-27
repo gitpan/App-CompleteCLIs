21bb419c98272166187c76acdd622cb4b1ae4fa3
perlancar <perlancar@gmail.com>
2014-12-27 03:35:14 +0000

    Import of PERLANCAR/App-CompleteCLIs-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: App-CompleteCLIs
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/App-CompleteCLIs-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

---
+++ lib/App/CompleteCLIs.pm
@@ -0,0 +1,62 @@
+package App::CompleteCLIs;
+
+our $DATE = '2014-12-27'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: CLI wrappers for complete_*() functions
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::CompleteCLIs - CLI wrappers for complete_*() functions
+
+=head1 VERSION
+
+This document describes version 0.01 of App::CompleteCLIs (from Perl distribution App-CompleteCLIs), released on 2014-12-27.
+
+=head1 SYNOPSIS
+
+=head1 SEE ALSO
+
+L<Complete>
+
+C<Complete::*> modules
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-CompleteCLIs>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-App-CompleteCLIs>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-CompleteCLIs>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
